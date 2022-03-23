8ad5cb43c1d205aaf5d7212216dfb981b6787839
Rita Alves <88343071+ritaalves95@users.noreply.github.com>
2022-03-23 14:51:52 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+            <div className="flex-1">
+                <div className="box-container">
+                    <h1>titulo</h1>
+                    <p>exemplo</p>
+                    <p>exemplo</p>
+                    <p>exemplo</p>
+                    <p>exemplo</p>
+                </div>
+
+                <div className="box-container">
+                    <h1>titulo</h1>
+                    <p>exemplo</p>
+                    <p>exemplo</p>
+                    <p>exemplo</p>
+                    <p>exemplo</p>
+                </div>
+            </div>
+
     </div>
   );
 }
