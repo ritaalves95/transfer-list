aa58f358dc49ce93c85adc3f6775667db682f7d6
Rita Alves <88343071+ritaalves95@users.noreply.github.com>
2022-03-23 15:02:57 +0000

    transfer list setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-            <div className="flex-1">
+            <div className="flex-wrapper">
                 <div className="box-container">
                     <h1>titulo</h1>
                     <p>exemplo</p>
@@ -11,6 +11,11 @@ function App() {
                     <p>exemplo</p>
                     <p>exemplo</p>
                 </div>
+
+                <article className="column-wrapper">
+                    <i className="fas fa-chevron-right"></i>
+                    <i className="fas fa-chevron-left"></i>
+                </article>
 
                 <div className="box-container">
                     <h1>titulo</h1>
